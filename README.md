dc5ac395adf2e551c551eb72dca1aeb620d3754e
{Krishna <gkrishnasree5@gmail.com>
2024-01-11 13:45:46 +0530

    Added sessions and cookies

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 POSTGRES QUERY TO CREATE TABLE: <br/><br/>
 
 CREATE TABLE users( <br/>
+	id SERIAL PRIMARY KEY, <br/>
 	email TEXT UNIQUE NOT NULL, <br/>
 	password TEXT NOT NULL <br/>
 );<br/><br/>
@@ -9,3 +10,4 @@ Level 1: verifying email and password from database.<br/>
 Level 2: environment variables.<br/>
 Level 3: Hashing passwords (using md5).<br/>
 Level 4: Salting and Hashing passwords (using bcrypt).<br/>
+Level 5: Sessions and Cookies (using express-session, passport, passport-local). <br/>
--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 import "dotenv/config";
-import bcrypt from "bcrypt";
 import express from "express";
 import bodyParser from "body-parser";
-import ejs from "ejs";
 import pg from "pg";
-import { error } from "console";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import localPassport from "passport-local";
+import session from "express-session";
 //import md5 from "md5";
 const saltRounds = 10;
-
-const app = express();
+ 
 const port = 3000;
-
-app.set('view engine', 'ejs');
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
-
+const app = express();
 const db = new pg.Client({
     user: process.env.USER,
     host: process.env.HOST,
@@ -22,80 +18,111 @@ const db = new pg.Client({
     password: process.env.PASSWORD,
     port: process.env.PORT,
 });
-
 db.connect();
-app.get("/",(req, res) => {
+const localStrategy = localPassport.Strategy;
+ 
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+ 
+passport.use("local-register",new localStrategy(async (username,password,done)=>{
+    try{
+        const result = await db.query("SELECT * FROM users WHERE email = $1",[username]);
+        const user = result.rows[0];
+        if(user!==undefined){
+            return done(null, false);
+        }
+        else{
+            const hashedPass = await bcrypt.hash(password, saltRounds);
+            await db.query("INSERT INTO users (email, password) VALUES ($1,$2)",[username,hashedPass]);
+            const newResult = await db.query("SELECT * FROM users WHERE email = $1",[username]);
+            const newUser = newResult.rows[0];
+            return done(null, newUser);
+        }
+    }
+    catch(error){
+        return done(error, false);   
+    }
+}));
+passport.use("local-login",new localStrategy(async (username,password,done)=>{
+    try{
+        const result = await db.query("SELECT * FROM users WHERE email = $1",[username]);
+        const user = result.rows[0];
+        if(user!==undefined){
+            const passCheck = await bcrypt.compare(password, user.password);
+            if(passCheck){
+                return done(null, user);
+            }
+            else{
+                return done(null, false);
+            }
+        }
+        else{
+            return done(null, false);
+        }
+    }
+    catch(error){
+        return done(error,false);
+    }   
+}));
+ 
+passport.serializeUser((user,done)=>{
+    done(null,user.id);
+});
+passport.deserializeUser(async (id,done)=>{
+    try{
+        const response = await db.query("SELECT * FROM users WHERE id = $1",[id]);
+        const user = response.rows[0];
+        done(null,user);
+    }
+    catch (error){
+        done(error,false);
+    }
+});
+ 
+app.get("/",async (req,res)=>{
     res.render("home.ejs");
 });
-
-app.get("/register",(req, res) => {
+ 
+app.get("/register",(req,res)=>{
     res.render("register.ejs");
 });
-
-app.post("/register",async (req, res) => {
-    const email=req.body.email;
-    const password = req.body.password;
-    try{
-        const result = await db.query("SELECT * FROM users WHERE email = $1",[email]);
-        
-        if(result.rows[0]){
-            console.log("User already exists!");
-            res.render("register.ejs");
-
-        }else{
-            bcrypt.hash(password, saltRounds, async function(err, hash) {
-                await db.query("INSERT INTO users VALUES ($1, $2)",[email, hash]);
-                console.log("User registered successfully!");
-                res.render("dest.ejs");
-            });
-            
-        }
-
-    }catch(err){
-        console.log(error);
-    }    
-});
-
-app.get("/login",(req, res) => {
+app.post("/register", passport.authenticate("local-register",{
+    failureRedirect: "/register",
+    successRedirect: "/dest",
+}));
+ 
+app.get("/login",(req,res)=>{
     res.render("login.ejs");
 });
-
-app.post("/login",async (req, res) => {
-    const email=req.body.email;
-    const password = req.body.password;
-    try{
-        const result = await db.query("SELECT * FROM users WHERE email = $1",[email]);
-
-        if(result.rows[0]){
-            const user = result.rows[0];
-            const hash = user.password;
-            bcrypt.compare(password, hash, function(err, result) {
-                
-                if(result){
-                    console.log("Login successful.");
-                    res.render("dest.ejs");
-                }else{
-                    console.log("Incorrect Password");
-                    res.render("login.ejs");
-                }
-
-            });
-
-        }else{
-            console.log("User not found!");
-            res.render("login.ejs");
-        }
-
-    }catch(err){
-        console.log(err);
+app.post("/login", passport.authenticate("local-login",{
+    failureRedirect: "/login",
+    successRedirect: "/dest",
+}));
+ 
+app.get("/dest",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("dest.ejs");
     }
-    
+    else{
+        res.redirect("/register");
+    }
 });
-
-app.get("/dest",(req, res) => {
-    res.render("dest.ejs");
+ 
+app.get("/logout",(req,res)=>{
+    res.clearCookie("connect.sid");    //this will clear the cookies left on client-side
+    req.logOut(()=>{
+        res.redirect("/");
+    });
 });
-
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
+ 
+app.listen(port,()=>{
+    console.log(`The server is running on port ${port}`);
 });
